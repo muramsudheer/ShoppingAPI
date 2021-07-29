c7b8918fc16c9655acb1c1bd7b7e8b716984450b
Mohamed Maazin Sudheer <mohammed.maazin@gmail.com>
2021-07-29 15:59:18 -0500

    Setup basic error handling

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
 const products = require('./api/routes/products');
 const orders = require('./api/routes/orders');
 
+app.use(morgan('dev'));
+
 app.use('/products', products);
 app.use('/orders', orders);
+
+app.use((req, res, next) => {
+    const error = new Error('Route not found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
 
 module.exports = app;
